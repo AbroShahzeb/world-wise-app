3f90e87b7df0abbe76d78b0770d3b4a14e3d8159
AbroShahzeb <khizr772@gmail.com>
2023-09-02 22:17:19 -0700

    Initial commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      WorldWide
+    </div>
+  );
+}
+
+export default Homepage;
